9d4a46f161c26fa365b383023c54a6db9fc92237
Eyang0612 <eddieyang@MacBook-Air-2.local>
2023-09-30 14:56:33 -0400

    modified Polynomial. java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,67 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+    
     Polynomial p = new Polynomial();
     System.out.println(p.evaluate(3));
-    double [] c1 = {6,0,0,5};
-    Polynomial p1 = new Polynomial(c1);
-    double [] c2 = {0,-2,0,0,-9};
-    Polynomial p2 = new Polynomial(c2);
+    double [] c1 = {6};
+    int [] c3 = {0};
+    Polynomial p1 = new Polynomial(c1, c3);
+    double [] c2 = {-5};
+    int [] c4 = {3};
+
+    Polynomial p2 = new Polynomial(c2,c4);
     Polynomial s = p1.add(p2);
     System.out.println("s(0.1) = " + s.evaluate(0.1));
+    
     if(s.hasRoot(1))
     System.out.println("1 is a root of s");
     else
     System.out.println("1 is not a root of s");
+
+    Polynomial p8 = new Polynomial();
+    Polynomial r = p8.add(s);
+    System.out.println("r(0.1) = " + r.evaluate(0.1));
+    if(r.hasRoot(1))
+    System.out.println("1 is a root of r");
+    else
+    System.out.println("1 is not a root of r");
+    
+    double [] c5 = {1.0};
+    int [] c6 = {1};
+    Polynomial p3 = new Polynomial(c5,c6);
+    Polynomial z = s.multiply(p3);
+    for (double a : z.Coefficients)
+            System.out.println(a);
+    for (int a : z.Exponents)
+            System.out.println(a);
+    System.out.println("///////");
+    System.out.println("z(0.1) = " + z.evaluate(0.1));
+    if(z.hasRoot(1))
+    System.out.println("1 is a root of z");
+    else
+    System.out.println("1 is not a root of z");
+    File file = new File("//Users//eddieyang//b07lab1//text");
+    Polynomial h = new Polynomial(file);
+    System.out.println("The Following are the Coefficients of h");
+    for (double i : h.Coefficients) System.out.println(i);
+     System.out.println("The Following are the Exponents of h");
+    for (int i : h.Exponents) System.out.println(i);
+    h.saveToFile("//Users//eddieyang//b07lab1//answer");
+
+
+
     }
+    
+    
+    
     }
--- Polynomial.java
+++ Polynomial.java
@@ -1,24 +1,88 @@
-
+import java.security.cert.PolicyQualifierInfo;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.function.IntFunction;
+import java.util.*;
+import java.util.ArrayList;
 
 public class Polynomial{
     double[] Coefficients;
+    int[] Exponents;
 
     public Polynomial(){
         Coefficients = new double[] {0.0};
+        Exponents = new int[] {0};
     }
 
-    public Polynomial(double[] a ){
+    public Polynomial(double[] a, int[] b){
         Coefficients = new double[a.length];
+        Exponents = new int[b.length];;
         for(int i = 0; i<a.length;i++){
             Coefficients[i]=a[i];
         }
+        for(int i = 0; i<b.length;i++){
+            Exponents[i]=b[i];
+        }
 
     }
+    
+    
 
     public Polynomial add(Polynomial a){
         Polynomial c = new Polynomial();
-        int len;
-        int shortlen;
+        int len = 0;
+        Polynomial d = new Polynomial(a.Coefficients,a.Exponents);
+        Polynomial e = new Polynomial(Coefficients, Exponents);
+        for(int i = 0; i<e.Exponents.length; i++){
+            for(int j = 0; j<d.Exponents.length; j++){
+                if(e.Exponents[i] == d.Exponents[j]){
+                    d.Exponents[j] = -1;
+                    len = len + 1;
+                    e.Coefficients[i] = e.Coefficients[i] + d.Coefficients[j];
+                }
+            }
+        }
+        for(int i = 0; i<e.Coefficients.length;i++){
+            if(e.Coefficients[i] == 0.0){
+                        e.Exponents[i] = -1;
+                        len = len +1;
+                    }
+        }
+        if((e.Exponents.length + d.Exponents.length - len) ==0){
+            return c;
+        }
+        c.Coefficients = new double[(e.Exponents.length + d.Exponents.length - len)];
+        c.Exponents = new int[(e.Coefficients.length + d.Coefficients.length - len)];
+        int digit = 0;
+        for(int i = 0; i<e.Exponents.length; i++){
+            if(e.Exponents[i] != -1){
+            c.Exponents[digit] = e.Exponents[i];
+            c.Coefficients[digit] = e.Coefficients[i];
+            digit = digit + 1;
+            }
+        }
+        for(int i = 0; i<d.Exponents.length; i++){
+            if(d.Exponents[i] != -1){
+                c.Exponents[digit] = d.Exponents[i];
+                c.Coefficients[digit] = d.Coefficients[i];
+                digit = digit + 1; 
+            }
+        }
+        return c;
+
+        
+
+
+        /*int shortlen;
         boolean model;
         if(a.Coefficients.length >= Coefficients.length){
             len = a.Coefficients.length;
@@ -44,7 +108,57 @@ public class Polynomial{
             }
 
         }
-        return c;
+        return c;*/
+        
+
+    }
+    public Polynomial(File a) throws IOException{
+        Scanner scan = new Scanner(a);
+        String line = scan.nextLine();
+        line = line.replaceAll("-","+-"); 
+        String[] content = line.split("\\+",line.length());
+    
+        Polynomial p = new Polynomial();
+        Polynomial q = new Polynomial();
+        String[] given;
+        double[] set_1;
+        int[] set_2;
+        for(int i = 0; i<content.length; i++){
+            if (content[i] == ""){
+                
+            }else{
+                given = content[i].split("x",2);
+                
+                if(given.length == 1){
+
+                    set_1 = new double[] {Double.parseDouble(given[0])};
+                    set_2 = new int[] {0};
+                    q = new Polynomial(set_1,set_2);
+                    
+                }else{
+                    if(given[0].isEmpty()){
+                        set_1 = new double[] {1};
+                    }else{
+                        set_1 = new double[] {Double.parseDouble(given[0])};
+                    }
+                    if(given[1].isEmpty()){
+                        set_2 = new int[] {1};
+                    }else{
+                        set_2 = new int[] {Integer.parseInt(given[1])};
+                    }
+                    q = new Polynomial(set_1,set_2);
+
+                }
+            
+                
+                p = p.add(q);
+            }
+        }
+        Coefficients = p.Coefficients;
+        Exponents = p.Exponents;
+        scan.close();
+
+
 
     }
 
@@ -53,7 +167,7 @@ public class Polynomial{
         double multi;
         for(int i = 0; i<Coefficients.length; i++){
             multi = 1;
-            for(int j = 0; j<i; j++){
+            for(int j = 0; j<Exponents[i]; j++){
                 multi = multi * x;
             }
             sum = sum + (Coefficients[i] * multi);
@@ -72,6 +186,57 @@ public class Polynomial{
 
 
     }
+    public Polynomial multiply(Polynomial a){
+        Polynomial c = new Polynomial();
+        Polynomial d = new Polynomial(new double[Coefficients.length], new int[Exponents.length]);
+        for(int j = 0; j<a.Exponents.length; j++){
+            d = new Polynomial(Coefficients, Exponents);
+            for(int i = 0; i<Exponents.length; i++){
+                d.Exponents[i] = d.Exponents[i] + a.Exponents[j];
+                d.Coefficients[i] = d.Coefficients[i] * a.Coefficients[j];
+            }
+            
+
+            c = c.add(d);
+        
+
+        }
+        return c;
+    }
+    public void saveToFile(String a) throws IOException {
+        String line = "";
+        FileWriter fw = new FileWriter(a);
+        BufferedWriter bw = new BufferedWriter(fw);
+        line = line.concat(String.valueOf(Coefficients[0]));
+            if(Exponents[0] != 0){
+                line = line.concat("x");
+                if(Exponents[0] != 1){
+                    line = line.concat(String.valueOf(Exponents[0]));
+                }
+            }
+        for(int i = 1; i<Coefficients.length; i++){
+            line = line.concat("+");
+            line = line.concat(String.valueOf(Coefficients[i]));
+            if(Exponents[i] != 0){
+                line = line.concat("x");
+                if(Exponents[i] != 1){
+                    line = line.concat(String.valueOf(Exponents[i]));
+                }
+            }
+
+
+        }
+        line = line.replaceAll("\\+-","-"); 
+        
+        
+        bw.write(line);
+        bw.close();
+
+
+
+
+    }
+
         
 
     
